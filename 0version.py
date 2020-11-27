2fb561e583aa08a695517d54ed30edc1d021efa4
HuangCongQing <1756260160@qq.com>
2020-11-27 20:42:46 +0800

    update

--- 0version.py
+++ 0version.py
@@ -4,8 +4,8 @@ Author: HCQ
 Company(School): UCAS
 Email: 1756260160@qq.com
 Date: 2020-10-20 20:50:01
-LastEditTime: 2020-10-24 15:53:42
-FilePath: /pytorch/01_torch_numpy.py
+LastEditTime: 2020-11-27 20:42:40
+FilePath: /pytorch/0version.py
 '''
 
 import torch
