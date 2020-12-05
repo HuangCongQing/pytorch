f79b897f7ef157ed568a6bb534dbadbab6c1e0ae
HuangCongQing <1756260160@qq.com>
2020-12-05 16:53:47 +0800

    add: 逻辑回归

---
+++ PyTorch深度学习实践/06逻辑回归.py
@@ -0,0 +1,9 @@
+'''
+Description: 逻辑回归
+Author: HCQ
+Company(School): UCAS
+Email: 1756260160@qq.com
+Date: 2020-12-05 16:53:13
+LastEditTime: 2020-12-05 16:53:21
+FilePath: /pytorch/PyTorch深度学习实践/06逻辑回归.py
+'''
