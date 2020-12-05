716f4135231c2b94a98bd84d0d629182015df533
HuangCongQing <1756260160@qq.com>
2020-12-05 16:54:51 +0800

    update

--- PyTorch深度学习实践/06逻辑回归.py
+++ PyTorch深度学习实践/06逻辑回归.py
@@ -1,9 +1,13 @@
 '''
 Description: 逻辑回归
+视频：https://www.bilibili.com/video/BV1Y7411d7Ys?p=6
+博客： https://blog.csdn.net/bit452/article/details/109680909
 Author: HCQ
 Company(School): UCAS
 Email: 1756260160@qq.com
 Date: 2020-12-05 16:53:13
-LastEditTime: 2020-12-05 16:53:21
+LastEditTime: 2020-12-05 16:54:43
 FilePath: /pytorch/PyTorch深度学习实践/06逻辑回归.py
 '''
+
+
