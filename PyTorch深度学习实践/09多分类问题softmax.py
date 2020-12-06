b1f615562a07cebb6451728ac1857d9e278ea662
HuangCongQing <1756260160@qq.com>
2020-12-06 20:44:02 +0800

    add: 多分类

--- PyTorch深度学习实践/09多分类问题softmax.py
+++ PyTorch深度学习实践/09多分类问题softmax.py
@@ -6,6 +6,87 @@ Author: HCQ
 Company(School): UCAS
 Email: 1756260160@qq.com
 Date: 2020-12-06 20:05:38
-LastEditTime: 2020-12-06 20:05:47
+LastEditTime: 2020-12-06 20:40:58
 FilePath: /pytorch/PyTorch深度学习实践/09多分类问题softmax.py
 '''
+import torch
+from torchvision import transforms
+from torchvision import datasets
+from torch.utils.data import DataLoader
+import torch.nn.functional as F
+import torch.optim as optim
+ 
+# 1 prepare dataset
+ 
+batch_size = 64
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]) # 归一化,均值和方差
+ 
+train_dataset = datasets.MNIST(root='./data/mnist/', train=True, download=True, transform=transform)
+train_loader = DataLoader(train_dataset, shuffle=True, batch_size=batch_size)
+test_dataset = datasets.MNIST(root='./data/mnist/', train=False, download=True, transform=transform)
+test_loader = DataLoader(test_dataset, shuffle=False, batch_size=batch_size)
+ 
+# 2 design model using class
+ 
+ 
+class Net(torch.nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.l1 = torch.nn.Linear(784, 512)
+        self.l2 = torch.nn.Linear(512, 256)
+        self.l3 = torch.nn.Linear(256, 128)
+        self.l4 = torch.nn.Linear(128, 64)
+        self.l5 = torch.nn.Linear(64, 10)
+ 
+    def forward(self, x):
+        x = x.view(-1, 784)  # -1其实就是自动获取mini_batch
+        x = F.relu(self.l1(x))
+        x = F.relu(self.l2(x))
+        x = F.relu(self.l3(x))
+        x = F.relu(self.l4(x))
+        return self.l5(x)  # 最后一层不做激活，不进行非线性变换
+ 
+ 
+model = Net()
+ 
+# 3 construct loss and optimizer
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+ 
+# 4 training cycle forward, backward, update
+ 
+ 
+def train(epoch):
+    running_loss = 0.0
+    for batch_idx, data in enumerate(train_loader, 0):
+        inputs, target = data
+        optimizer.zero_grad()
+ 
+        outputs = model(inputs)
+        loss = criterion(outputs, target)
+        loss.backward()
+        optimizer.step()
+ 
+        running_loss += loss.item()
+        if batch_idx % 300 == 299:
+            print('[%d, %5d] loss: %.3f' % (epoch+1, batch_idx+1, running_loss/300))
+            running_loss = 0.0
+ 
+ 
+def test():
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in test_loader:
+            images, labels = data
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, dim=1) # dim = 1 列是第0个维度，行是第1个维度
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item() # 张量之间的比较运算
+    print('accuracy on test set: %d %% ' % (100*correct/total))
+ 
+ 
+if __name__ == '__main__':
+    for epoch in range(10):
+        train(epoch)
+        test()
