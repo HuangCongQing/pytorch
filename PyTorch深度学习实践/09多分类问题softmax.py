1d65d809e033c7dab12b46bdc9421dbd081af09f
HuangCongQing <1756260160@qq.com>
2020-12-06 20:05:57 +0800

    update

---
+++ PyTorch深度学习实践/09多分类问题softmax.py
@@ -0,0 +1,11 @@
+'''
+Description: 多分类问题(softmax)
+视频：https://www.bilibili.com/video/BV1Y7411d7Ys?p=9
+博客：https://blog.csdn.net/bit452/article/details/109686936
+Author: HCQ
+Company(School): UCAS
+Email: 1756260160@qq.com
+Date: 2020-12-06 20:05:38
+LastEditTime: 2020-12-06 20:05:47
+FilePath: /pytorch/PyTorch深度学习实践/09多分类问题softmax.py
+'''
