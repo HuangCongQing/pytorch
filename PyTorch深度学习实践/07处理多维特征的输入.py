6e66b0ac18d281f5be84df0d82c6b8b796ac6265
HuangCongQing <1756260160@qq.com>
2020-12-06 15:46:43 +0800

    update

---
+++ PyTorch深度学习实践/07处理多维特征的输入.py
@@ -0,0 +1,15 @@
+'''
+Description: 处理多维特征的输入
+视频：https://www.bilibili.com/video/BV1Y7411d7Ys?p=7
+博客：
+https://blog.csdn.net/bit452/article/details/109682078
+https://blog.csdn.net/weixin_44841652/article/details/105125826
+Author: HCQ
+Company(School): UCAS
+Email: 1756260160@qq.com
+Date: 2020-12-06 12:37:48
+LastEditTime: 2020-12-06 12:37:57
+FilePath: /pytorch/PyTorch深度学习实践/07处理多维特征的输入.py
+'''
+
+
