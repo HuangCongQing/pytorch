54e4b737c47b873588f674b8562839017b782849
HuangCongQing <1756260160@qq.com>
2020-12-07 12:06:59 +0800

    add: CNN实现

---
+++ PyTorch深度学习实践/10.5简单CNN.py
@@ -0,0 +1,96 @@
+'''
+Description: CNN实现
+视频：https://www.bilibili.com/video/BV1Y7411d7Ys?p=10
+博客：
+https://blog.csdn.net/bit452/article/details/109690712
+https://blog.csdn.net/weixin_44841652/article/details/105243630
+Author: HCQ
+Company(School): UCAS
+Email: 1756260160@qq.com
+Date: 2020-12-07 11:52:21
+LastEditTime: 2020-12-07 12:06:49
+FilePath: /pytorch/PyTorch深度学习实践/10.5简单CNN.py
+'''
+import torch
+from torchvision import transforms
+from torchvision import datasets
+from torch.utils.data import DataLoader
+import torch.nn.functional as F
+import torch.optim as optim
+ 
+# prepare dataset
+ 
+batch_size = 64
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
+ 
+train_dataset = datasets.MNIST(root='./data/mnist/', train=True, download=True, transform=transform)
+train_loader = DataLoader(train_dataset, shuffle=True, batch_size=batch_size)
+test_dataset = datasets.MNIST(root='./data/mnist/', train=False, download=True, transform=transform)
+test_loader = DataLoader(test_dataset, shuffle=False, batch_size=batch_size)
+ 
+# design model using class
+ 
+ 
+class Net(torch.nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = torch.nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = torch.nn.Conv2d(10, 20, kernel_size=5)
+        self.pooling = torch.nn.MaxPool2d(2)
+        self.fc = torch.nn.Linear(320, 10)
+ 
+ 
+    def forward(self, x):
+        # flatten data from (n,1,28,28) to (n, 784)
+        batch_size = x.size(0)  # 
+        x = F.relu(self.pooling(self.conv1(x)))
+        x = F.relu(self.pooling(self.conv2(x)))
+        x = x.view(batch_size, -1) # -1 此处自动算出的是320
+        x = self.fc(x)
+ 
+        return x
+ 
+ 
+model = Net()
+ 
+# construct loss and optimizer
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+ 
+# training cycle forward, backward, update
+ 
+ 
+def train(epoch):
+    running_loss = 0.0
+    for batch_idx, data in enumerate(train_loader, 0):
+        inputs, target = data
+        optimizer.zero_grad()
+ 
+        outputs = model(inputs)
+        loss = criterion(outputs, target)
+        loss.backward()
+        optimizer.step()
+ 
+        running_loss += loss.item()
+        if batch_idx % 300 == 299:
+            print('[%d, %5d] loss: %.3f' % (epoch+1, batch_idx+1, running_loss/300))
+            running_loss = 0.0
+ 
+ 
+def test():
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in test_loader:
+            images, labels = data
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, dim=1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    print('accuracy on test set: %d %% ' % (100*correct/total))
+ 
+ 
+if __name__ == '__main__':
+    for epoch in range(10):
+        train(epoch)
+        test()
