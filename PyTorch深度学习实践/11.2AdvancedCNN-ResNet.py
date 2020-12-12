bd5a4a45d4f9cb997aedd52ac4802853d0b5e8cf
HuangCongQing <1756260160@qq.com>
2020-12-12 15:28:29 +0800

    add: ResNet

---
+++ PyTorch深度学习实践/11.2AdvancedCNN-ResNet.py
@@ -0,0 +1,110 @@
+'''
+Description: ResNet
+Author: HCQ
+Company(School): UCAS
+Email: 1756260160@qq.com
+Date: 2020-12-12 15:25:16
+LastEditTime: 2020-12-12 15:28:13
+FilePath: /pytorch/PyTorch深度学习实践/11.2AdvancedCNN-ResNet.py
+'''
+
+
+import torch
+import torch.nn as nn
+from torchvision import transforms
+from torchvision import datasets
+from torch.utils.data import DataLoader
+import torch.nn.functional as F
+import torch.optim as optim
+ 
+# prepare dataset
+ 
+batch_size = 64
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]) # 归一化,均值和方差
+ 
+train_dataset = datasets.MNIST(root='./data/mnist/', train=True, download=True, transform=transform)
+train_loader = DataLoader(train_dataset, shuffle=True, batch_size=batch_size)
+test_dataset = datasets.MNIST(root='./data/mnist/', train=False, download=True, transform=transform)
+test_loader = DataLoader(test_dataset, shuffle=False, batch_size=batch_size)
+ 
+# design model using class
+class ResidualBlock(nn.Module):
+    def __init__(self, channels):
+        super(ResidualBlock, self).__init__()
+        self.channels = channels
+        self.conv1 = nn.Conv2d(channels, channels, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(channels, channels, kernel_size=3, padding=1)
+ 
+    def forward(self, x):
+        y = F.relu(self.conv1(x))
+        y = self.conv2(y)
+        return F.relu(x + y)
+ 
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 16, kernel_size=5)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=5) # 88 = 24x3 + 16
+ 
+        self.rblock1 = ResidualBlock(16)
+        self.rblock2 = ResidualBlock(32)
+ 
+        self.mp = nn.MaxPool2d(2)
+        self.fc = nn.Linear(512, 10) # 512咋能自动出来的
+ 
+ 
+    def forward(self, x):
+        in_size = x.size(0)
+ 
+        x = self.mp(F.relu(self.conv1(x)))
+        x = self.rblock1(x)
+        x = self.mp(F.relu(self.conv2(x)))
+        x = self.rblock2(x)
+ 
+        x = x.view(in_size, -1)
+        x = self.fc(x)
+        return x
+ 
+model = Net()
+ 
+# construct loss and optimizer
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+ 
+# training cycle forward, backward, update
+ 
+ 
+def train(epoch):
+    running_loss = 0.0
+    for batch_idx, data in enumerate(train_loader, 0):
+        inputs, target = data
+        optimizer.zero_grad()
+ 
+        outputs = model(inputs)
+        loss = criterion(outputs, target)
+        loss.backward()
+        optimizer.step()
+ 
+        running_loss += loss.item()
+        if batch_idx % 300 == 299:
+            print('[%d, %5d] loss: %.3f' % (epoch+1, batch_idx+1, running_loss/300))
+            running_loss = 0.0
+ 
+ 
+def test():
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in test_loader:
+            images, labels = data
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, dim=1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    print('accuracy on test set: %d %% ' % (100*correct/total))
+ 
+ 
+if __name__ == '__main__':
+    for epoch in range(10):
+        train(epoch)
+        test()
