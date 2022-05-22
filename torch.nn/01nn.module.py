f17975a6afa855744cb76ba414ac9406373f0825
hcq <1756260160@qq.com>
2022-05-22 11:58:21 +0800

    add

--- torch.nn/01nn.module.py
+++ torch.nn/01nn.module.py
@@ -1,2 +1,18 @@
+# import torch
+# print(torch.cuda.is_available())
 import torch
-print(torch.cuda.is_available())
+from torch import nn
+
+class Mymodule(nn.Module):
+    def __init__(self):
+        super(Mymodule, self).__init__()
+
+    def forward(self, input):
+        output = input + 1
+        return output
+
+
+mymodule = Mymodule() # 调用__init__
+x = torch.tensor(1.0)
+output = mymodule(x) # 调用forward
+print(output)
